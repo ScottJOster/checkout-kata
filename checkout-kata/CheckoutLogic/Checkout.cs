5ffb2f49f5813ae5397c31b960c98761001d42ca
ScottODev <101461116+ScottJOster@users.noreply.github.com>
2024-04-12 17:42:22 +0100

    add basic checkout class and extract interface

---
+++ checkout-kata/CheckoutLogic/Checkout.cs
@@ -0,0 +1,20 @@
+﻿using checkout_kata.Products;
+
+namespace checkout_kata.CheckoutLogic
+{
+    public class Checkout : ICheckout
+    {
+        public List<Product> Basket { get; set; } = new List<Product>();
+
+
+        public void Scan(Product product)
+        {
+            Basket.Add(product);
+        }
+
+        public double GetTotalPrice()
+        {
+            return 1;
+        }
+    }
+}
---
+++ checkout-kata/CheckoutLogic/ICheckout.cs
@@ -0,0 +1,12 @@
+﻿using checkout_kata.Products;
+
+namespace checkout_kata.CheckoutLogic
+{
+    public interface ICheckout
+    {
+        List<Product> Basket { get; set; }
+
+        double GetTotalPrice();
+        void Scan(Product product);
+    }
+}
