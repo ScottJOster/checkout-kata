6225a37454278ddf0b9c8533964b94f8f7063a8a
ScottODev <101461116+ScottJOster@users.noreply.github.com>
2024-04-12 18:08:19 +0100

    As a developer I want to avoid hard coding magic numbers and strings and save time - create constants

--- UnitTests/CheckoutTests.cs
+++ UnitTests/CheckoutTests.cs
@@ -1,12 +1,10 @@
 using checkout_kata.CheckoutLogic;
+using checkout_kata.Constants;
 using checkout_kata.Products;
 using FluentAssertions;
 
 namespace UnitTests
 {
-
-
-
     [TestClass]
     public class CheckoutTests
     {
@@ -30,6 +28,21 @@ namespace UnitTests
             checkout.Basket.Should().NotBeEmpty();
             checkout.Basket.Count.Should().Be(1);
             checkout.Basket[0].Name.Should().Be("Product A");
+        }
+
+        [TestMethod]
+        public void GetTotalPrice_ShouldReturnTotalPriceOfItemsInBasket()
+        {
+            //Arrange 
+            var productA = ProductDictionary.Products[ProductConstants.ProductA];
+            var checkout = new Checkout();
+            checkout.Scan(productA);
+            //Act 
+            var result = checkout.GetTotalPrice();
+            //Assert 
+            checkout.Basket.Should().NotBeEmpty();
+            checkout.Basket.Count.Should().Be(1);
+            checkout.Basket[0].Name.Should().Be(ProductConstants.ProductAName);
         }
     }
 }
---
+++ checkout-kata/Constants/ProductConstants.cs
@@ -0,0 +1,30 @@
+﻿namespace checkout_kata.Constants
+{
+    public static class ProductConstants
+    {
+        //Names
+        public const string ProductAName  = "Product A";
+        public const string ProductBName = "Product B";
+        public const string ProductCName = "Product C";
+        public const string ProductDName = "Product D";
+        //Keys
+        public const string ProductA = "A";
+        public const string ProductB = "B";
+        public const string ProductC = "C";
+        public const string ProductD = "D";
+        //Prices
+        public const double ProductAPrice = 50;
+        public const double ProductBPrice = 30;
+        public const double ProductCPrice = 20;
+        public const double ProductDPrice = 15;
+        //Quantities where discount is applicable
+        public const int ADsicountQty = 3;
+        public const int BDiscountQty = 2;
+        //Discount amounts
+        public const double ADsicountAmount = 20;
+        public const double BDiscountAmount = 15;
+
+
+
+    }
+}
