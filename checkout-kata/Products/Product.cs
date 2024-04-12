956c95ea4ad917f49bd56069395d969a3546e553
ScottODev <101461116+ScottJOster@users.noreply.github.com>
2024-04-12 17:01:27 +0100

    As a developer I want to be able to easily configure the product properties and avoid hardcoding - add product model and dictionary

---
+++ checkout-kata/Products/Product.cs
@@ -0,0 +1,10 @@
+﻿namespace checkout_kata.Products
+{
+    public class Product
+    {
+        public string Name { get; set; } = "MiscGoods";
+        public double PriceGbp { get; set; }
+        public int? DiscountApplicableQuantity { get; set; }
+        public double? DiscountAmount {  get; set; }
+    }
+}
---
+++ checkout-kata/Products/ProductDictionary.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace checkout_kata.Products
+{
+    public class ProductDictionary
+    {
+        /// <summary>
+        /// Dictionary of products and required fields simulating configurable entities in a DB
+        /// </summary>
+        public IDictionary<string, Product> Products = new Dictionary<string, Product> 
+        {
+            { "A", new Product{Name = "Product A", PriceGbp = 50, DiscountApplicableQuantity = 3, DiscountAmount = 20} },
+            { "B", new Product{Name = "Product B", PriceGbp = 30, DiscountApplicableQuantity = 2, DiscountAmount = 15} },
+            { "C", new Product{Name = "Product C", PriceGbp = 20} },
+            { "D", new Product{Name = "Product D", PriceGbp = 15} }
+
+        };
+    }
+}
